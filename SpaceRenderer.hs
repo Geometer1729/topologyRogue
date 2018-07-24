b73b58447df0a81f8b06770b8ea99f39edcdee9d
Josh Gordon <joshua.sam.gordon@gmail.com>
2018-07-24 01:05:50 -0400

    Does something to space renderer

--- SpaceRenderer.hs
+++ SpaceRenderer.hs
@@ -24,23 +24,23 @@ renderGrid n w h = let horizontal = [[(n*i,-w/2),(n*i,w/2)] | i <- [-w/(2*n)..w/
                        vertical = [[(-h/2,n*i),(h/2,n*i)] | i <- [-h/(2*n)..h/(2*n)]] :: [[(Float,Float)]]
                    in Pictures $ map Line (horizontal ++ vertical)
 
-type World = (Space,[(Object,(Float,Float))])
+type World = (Space,[(Object,Location)])
 
 renderWorld :: World -> Picture
 renderWorld w@(s,o) = let grid = renderGrid 10 500 500
-                          pieces = [spaceDraw s (fst p) | p <- o]
+                          pieces = [spaceDraw s (move (snd p) (fst p)) | p <- o]
                       in Pictures (pieces ++ [grid])
 
-testWorld = ((t2 250 250),[(testob,(200 :: Float,300 :: Float))])
+testWorld = ((t2 250 250),[(testob,((0::Float,0::Float),(False,0::Float)))])
 
 handleEventWorld :: Event -> World -> World
 handleEventWorld (EventKey k downkey mods f@(x,y)) t@(s,os) = case k of
                                               (Char m) -> if isDown downkey then trace (show m) t  else t
                                               (MouseButton LeftButton) -> trace (show x ++ "," ++ show y) t
-                                              (SpecialKey KeyUp) -> (s,[(move (up 10) (fst o),snd o) | o <- os])
-                                              (SpecialKey KeyDown) -> (s,[(move (down 10) (fst o),snd o) | o <- os])
-                                              (SpecialKey KeyLeft) -> (s,[(move (left 10) (fst o),snd o) | o <- os])
-                                              (SpecialKey KeyRight) -> (s,[(move (right 10) (fst o),snd o) | o <- os])
+                                              (SpecialKey KeyUp) -> (s,[(fst o,comp (up 10) (snd o)) | o <- os])
+                                              (SpecialKey KeyDown) -> (s,[(fst o,comp (down 10) (snd o)) | o <- os])
+                                              (SpecialKey KeyLeft) -> (s,[(fst o,comp (left 10) (snd o)) | o <- os])
+                                              (SpecialKey KeyRight) -> (s,[(fst o,comp (right 10) (snd o)) | o <- os])
 handleEventWorld _ t = t
 
 stepWorld :: Float -> World -> World

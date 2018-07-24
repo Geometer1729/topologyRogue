4a0dafebfb98387fda00e9944d14e07339d17593
Josh Gordon <joshua.sam.gordon@gmail.com>
2018-07-24 00:01:41 -0400

    Add player controls

--- Main.hs
+++ Main.hs
@@ -5,28 +5,8 @@ import Debug.Trace
 import Object
 import SpaceRenderer
 
-type TestWorld = [[Int]]
 
-world :: TestWorld
-world = [[1..s] | s <- [2..11]]
-
-isDown :: KeyState -> Bool
-isDown Down = True
-isDown _ = False
-
-handleEvent :: Event -> TestWorld -> TestWorld
-handleEvent (EventKey k down mods f@(x,y)) t = case k of
-                                              (Char m) -> if isDown down then trace (show m) t  else t
-                                              (MouseButton LeftButton) -> trace (show x ++ "," ++ show y) t
-                                              (SpecialKey _) -> t
-handleEvent _ t = t
-
-stepWorld :: Float -> TestWorld -> TestWorld
-stepWorld _ t = t
-
-testRender :: TestWorld -> Picture
-testRender t = renderGrid 10 500 500
 
 main :: IO ()
-main = --play (InWindow "Nice Window" (500,500) (0, 0)) white 1 world testRender handleEvent stepWorld
-       display (InWindow "Nice Window" (500,500) (0, 0)) white (renderWorld testWorld)
+main = play (InWindow "Nice Window" (500,500) (0, 0)) white 1 testWorld renderWorld handleEventWorld stepWorld
+       --display (InWindow "Nice Window" (500,500) (0, 0)) white (renderWorld testWorld)
--- SpaceRenderer.hs
+++ SpaceRenderer.hs
@@ -1,8 +1,23 @@
 module SpaceRenderer where
+import Graphics.Gloss.Interface.Pure.Game
 import Graphics.Gloss
 import Object
 import Space
+import Debug.Trace
 
+isDown :: KeyState -> Bool
+isDown Down = True
+isDown _ = False
+
+--Locations for moving
+up :: Float -> Location
+up x = ((0,x),(False,0 :: Float))
+down :: Float -> Location
+down x = ((0,-x),(False,0 :: Float))
+left :: Float -> Location
+left x = ((-x,0),(False,0 :: Float))
+right :: Float -> Location
+right x = ((x,0),(False,0 :: Float))
 
 renderGrid :: Float -> Float -> Float -> Picture
 renderGrid n w h = let horizontal = [[(n*i,-w/2),(n*i,w/2)] | i <- [-w/(2*n)..w/(2*n)]] :: [[(Float,Float)]]
@@ -16,4 +31,17 @@ renderWorld w@(s,o) = let grid = renderGrid 10 500 500
                           pieces = [spaceDraw s (fst p) | p <- o]
                       in Pictures (pieces ++ [grid])
 
-testWorld = ((t2 500 500),[(testob,(200 :: Float,300 :: Float))])
+testWorld = ((t2 250 250),[(testob,(200 :: Float,300 :: Float))])
+
+handleEventWorld :: Event -> World -> World
+handleEventWorld (EventKey k downkey mods f@(x,y)) t@(s,os) = case k of
+                                              (Char m) -> if isDown downkey then trace (show m) t  else t
+                                              (MouseButton LeftButton) -> trace (show x ++ "," ++ show y) t
+                                              (SpecialKey KeyUp) -> (s,[(move (up 10) (fst o),snd o) | o <- os])
+                                              (SpecialKey KeyDown) -> (s,[(move (down 10) (fst o),snd o) | o <- os])
+                                              (SpecialKey KeyLeft) -> (s,[(move (left 10) (fst o),snd o) | o <- os])
+                                              (SpecialKey KeyRight) -> (s,[(move (right 10) (fst o),snd o) | o <- os])
+handleEventWorld _ t = t
+
+stepWorld :: Float -> World -> World
+stepWorld _ = id

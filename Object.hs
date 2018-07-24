bdd1a088ff3f9aa431d53f7e9fb41850f40f8898
Geometer1729 <kuhnsbt@clarkson.edu>
2018-07-23 20:15:03 -0400

    make polygons solid

--- Object.hs
+++ Object.hs
@@ -14,7 +14,7 @@ testob::Object
 testob =[([(10,10),(10,80),(80,80)],black),([(100,100),(100,180),(180,180)],red)]
 
 objectToPicture :: Object -> Picture
-objectToPicture o = Pictures $ map (\ (xs,c) -> color c $ Line xs) o
+objectToPicture o = Pictures $ map (\ (xs,c) -> color c $ Polygon xs) o
 
 render::Object -> IO ()
 render o = display (InWindow "Nice Window" (500,500) (0, 0)) white (objectToPicture o)
